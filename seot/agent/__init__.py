fc108db93d70d003266d77bc1ed654b42e660327
Keichi Takahashi <keichi.t@me.com>
2016-10-12 18:39:46 +0900

    起動時に基本情報を出力

--- seot/agent/__init__.py
+++ seot/agent/__init__.py
@@ -1,9 +1,29 @@
 import asyncio
+import logging
 
 
 import colorlog
 from seot.agent import config
 import uvloop
+
+
+__version__ = "0.0.1"
+
+logger = logging.getLogger("core")
+logger.setLevel(logging.INFO)
+
+
+def print_startup_message():
+    logger.info("Launching SEoT Agent version {0}".format(__version__))
+    logger.info("Owner of this device: {0}".format(config.get("user_id")))
+    logger.info("Device type: {0}".format(config.get("type")))
+    logger.info("Device coordinate: ({0}, {1})".format(
+        config.get("coordinate.longitude"),
+        config.get("coordinate.latitude")
+    ))
+    logger.info("Heartbeat interval: {0}s".format(
+        config.get("heartbeat_interval"))
+    )
 
 
 def main():
@@ -17,4 +37,5 @@ def main():
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
     config.init()
-    print(config.get(""))
+
+    print_startup_message()
