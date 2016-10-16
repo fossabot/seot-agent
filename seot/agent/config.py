44ee8e0997e9f0cf045cc2f5e04a94b459f802fa
Keichi Takahashi <keichi.t@me.com>
2016-10-16 15:32:36 +0900

    設定ファイルの設定値をデフォルト値とマージするようにした

--- seot/agent/config.py
+++ seot/agent/config.py
@@ -3,28 +3,51 @@ import sys
 import uuid
 from pathlib import Path
 
-import kaptan
-
 from seot import agent
+import yaml
 
 CONFIG_FILE_PATH = Path.home() / ".config/seot/config.yml"
 SEOT_DIR_PATH = Path.home() / ".local/share/seot"
 STATE_FILE_PATH = SEOT_DIR_PATH / "state.yml"
 
 logger = logging.getLogger(__name__)
-_config = None
-_state = None
+_config = {
+    "cpp": {
+        "heartbeat_interval": "60",
+        "base_url": "http://localhost:8888/api"
+    },
+    "dpp": {
+        "listen_address": "0.0.0.0",
+        "listen_port": "51423"
+    },
+    "docker": {
+        "base_url": "tcp://localhost:1234"
+    }
+}
+_state = {}
+
+
+def _get(config, key):
+    current = config
+
+    for component in key.split("."):
+        if component in current:
+            current = current.get(component)
+        else:
+            return None
+
+    return current
 
 
 def get(key=None):
     """ Get a configuration value """
-    return _config.get(key)
+    return _get(_config, key)
 
 
 def save_state():
     """ Persis current state """
     with STATE_FILE_PATH.open("w") as f:
-        f.write(_state.export())
+        f.write(yaml.dump(_state))
 
 
 def _init_state():
@@ -43,7 +66,38 @@ def _init_state():
 
 def get_state(key=None):
     """ Get a state value """
-    return _state.get(key)
+    return _get(_state, key)
+
+
+def _merge_dict(dst, src):
+    for k, v in src.items():
+        if isinstance(v, dict):
+            dst.setdefault(k, {})
+            _merge_dict(dst[k], v)
+        else:
+            dst[k] = v
+
+    return dst
+
+
+def _validate_config():
+    failed = False
+
+    if get("device.user_id") is None:
+        logger.error("device.user_id is a required config value")
+        failed = True
+    if get("device.type") is None:
+        logger.error("device.type is a required config value")
+        failed = True
+    if get("device.coordinate.longitude") is None:
+        logger.error("device.coordinate.longitude is a required config value")
+        failed = True
+    if get("device.coordinate.latitude") is None:
+        logger.error("device.coordinate.latitude is a required config value")
+        failed = True
+
+    if failed:
+        sys.exit(1)
 
 
 def load():
@@ -51,23 +105,26 @@ def load():
     global _config
     global _state
 
-    try:
-        logger.info("Loading configurations from {0}".format(CONFIG_FILE_PATH))
-        _config = kaptan.Kaptan(handler="yaml")
-        _config.import_config(str(CONFIG_FILE_PATH))
-        logger.info("Configurations successfully loaded")
-    except:
-        logger.error("Failed to load configurations")
-        sys.exit(1)
+    if CONFIG_FILE_PATH.exists():
+        try:
+            logger.info("Loading configurations from {0}".format(
+                CONFIG_FILE_PATH
+            ))
+            with open(str(CONFIG_FILE_PATH)) as f:
+                _merge_dict(_config, yaml.load(f))
+            logger.info("Configurations successfully loaded")
+        except:
+            logger.error("Failed to load configurations")
+            sys.exit(1)
 
-    _state = kaptan.Kaptan(handler="yaml")
     if STATE_FILE_PATH.exists():
         try:
             logger.info("Loading states from {0}".format(STATE_FILE_PATH))
-            _state.import_config(str(STATE_FILE_PATH))
+            with open(str(STATE_FILE_PATH)) as f:
+                _merge_dict(_state, yaml.load(f))
             logger.info("States successfully loaded")
         except:
             logger.error("Failed to load state")
             sys.exit(1)
-    else:
-        _init_state()
+
+    _validate_config()
