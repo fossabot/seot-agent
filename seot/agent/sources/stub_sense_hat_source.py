ef9abcc8dad557c41fff120f0ba55fd1b11e0ac6
Keichi Takahashi <keichi.t@me.com>
2016-12-06 16:28:04 +0900

    StubSenseHatSourceを追加

---
+++ seot/agent/sources/stub_sense_hat_source.py
@@ -0,0 +1,38 @@
+import asyncio
+import logging
+import random
+
+from . import BaseSource
+
+logger = logging.getLogger(__name__)
+
+
+class StubSenseHatSource(BaseSource):
+    def __init__(self, interval=5, **kwargs):
+        super().__init__(**kwargs)
+        self.interval = interval
+
+        # These synthetic values are generated based on Wiener process
+        self.temperature = 25.0
+        self.humidity = 50.0
+        self.pressure = 1013.0
+
+    async def startup(self):
+        logger.info("Starting StubSenseHatSource...")
+
+    async def _run(self):
+        while True:
+            sigma = self.interval / 100.0
+
+            self.temperature = random.gauss(self.temperature, sigma)
+            self.humidity = random.gauss(self.humidity, sigma)
+            self.pressure = random.gauss(self.pressure, sigma)
+
+            data = {
+                "temperature": self.temperature,
+                "humidity": self.humidity,
+                "pressure": self.pressure
+            }
+            await self._emit(data)
+
+            await asyncio.sleep(self.interval, loop=self.loop)
