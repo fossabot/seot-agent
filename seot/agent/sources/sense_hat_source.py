4749c7087bd9f3cde0dfe447ffae4e470744f4ff
Keichi Takahashi <keichi.t@me.com>
2016-12-19 17:55:09 +0900

    SenseHatSourceを追加

---
+++ seot/agent/sources/sense_hat_source.py
@@ -0,0 +1,28 @@
+import asyncio
+import logging
+
+from sense_hat import SenseHat
+from . import BaseSource
+
+logger = logging.getLogger(__name__)
+
+
+class StubSenseHatSource(BaseSource):
+    def __init__(self, interval=5, **kwargs):
+        super().__init__(**kwargs)
+        self.interval = interval
+        self.sense = SenseHat()
+
+    async def startup(self):
+        logger.info("Starting SenseHatSource...")
+
+    async def _run(self):
+        while True:
+            data = {
+                "temperature": self.sense.get_temperature(),
+                "humidity": self.sense.get_humidity(),
+                "pressure": self.sense.get_pressure()
+            }
+            await self._emit(data)
+
+            await asyncio.sleep(self.interval, loop=self.loop)
