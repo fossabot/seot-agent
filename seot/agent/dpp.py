3defc1a7c92b66d8d62b98a65cf4e5dd4cb51c63
Keichi Takahashi <keichi.t@me.com>
2017-01-19 19:04:47 +0900

    中間処理コンテナ用のAPIを追加

--- seot/agent/dpp.py
+++ seot/agent/dpp.py
@@ -1,4 +1,9 @@
+import socket
+
 import msgpack
+
+
+SOCK_PATH = "/tmp/seot.sock"
 
 
 def encode(data):
@@ -7,3 +12,17 @@ def encode(data):
 
 def decode(data):
     return msgpack.unpackb(data, encoding="utf-8")
+
+
+def run_handler(handler):
+    unpacker = msgpack.Unpacker(encoding="utf-8")
+
+    with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
+        sock.connect(SOCK_PATH)
+
+        while True:
+            buf = sock.recv(1024)
+            unpacker.feed(buf)
+            for msg in unpacker:
+                output = handler(msg)
+                sock.send(encode(output))
