16cf8fc44efaa639aa52f9b06ae4ad38ad20230b
Keichi Takahashi <keichi.t@me.com>
2017-02-07 18:11:19 +0900

    DebugSinkがbytesを受け取っても落ちないようにした

--- seot/agent/sinks/__init__.py
+++ seot/agent/sinks/__init__.py
@@ -1,4 +1,5 @@
 import asyncio
+import collections
 import json
 import logging
 from abc import abstractmethod
@@ -33,8 +34,18 @@ class DebugSink(BaseSink):
         super().__init__(**kwargs)
         self.level = level
 
+    def _sanitize(self, data):
+        if isinstance(data, bytes):
+            return "<binary data ({0} bytes)>".format(len(data))
+        elif isinstance(data, collections.Mapping):
+            return dict(map(self._sanitize, data.items()))
+        elif isinstance(data, collections.Iterable):
+            return type(data)(map(self._sanitize, data))
+        else:
+            return data
+
     async def _process(self, data):
-        formatted_json = json.dumps(data, indent=4)
+        formatted_json = json.dumps(self._sanitize(data), indent=4)
         colorful_json = highlight(formatted_json, lexers.JsonLexer(),
                                   formatters.TerminalFormatter())
         logger.log(self.level, "{0} received:\n{1}".format(
