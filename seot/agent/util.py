03c7ecce77505bfe8333cec6b4629c0058e40e1a
Keichi Takahashi <keichi.t@me.com>
2017-02-14 10:55:01 +0900

    起動時のログにプラットフォーム関係の情報も出すように

--- seot/agent/util.py
+++ seot/agent/util.py
@@ -24,9 +24,16 @@ def log_startup_message():
         config.get("agent.coordinate.longitude"),
         config.get("agent.coordinate.latitude")
     ))
-    logger.info("Heartbeat interval: {0}s".format(
+    logger.info("Agent heartbeat interval: {0}s".format(
         config.get("cpp.heartbeat_interval"))
     )
+    logger.info("Machine hostname: {0}".format(config.get("facts.hostname")))
+    logger.info("Machine OS: {0} {1} ({2})".format(
+        config.get("facts.kernel"),
+        config.get("facts.os"),
+        config.get("facts.arch")
+    ))
+    logger.info("Machine IP: {0}".format(config.get("facts.ip")))
 
 
 def log_quit_message():
