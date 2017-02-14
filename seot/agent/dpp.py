fb5c52413ce9e21469eaa269d6f1bb2fa4654a57
Keichi Takahashi <keichi.t@me.com>
2017-02-14 14:40:31 +0900

    seot.agent.dppから中間処理コンテナ関連のコードを削除

--- seot/agent/dpp.py
+++ seot/agent/dpp.py
@@ -1,9 +1,4 @@
-import socket
-
 import msgpack
-
-
-SOCK_PATH = "/tmp/seot.sock"
 
 
 def encode(data):
@@ -12,17 +7,3 @@ def encode(data):
 
 def decode(data):
     return msgpack.unpackb(data, encoding="utf-8")
-
-
-def run_handler(handler):
-    unpacker = msgpack.Unpacker(encoding="utf-8")
-
-    with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
-        sock.connect(SOCK_PATH)
-
-        while True:
-            buf = sock.recv(1024)
-            unpacker.feed(buf)
-            for msg in unpacker:
-                output = handler(msg)
-                sock.send(encode(output))
